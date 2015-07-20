b5c4679b8661df23f3fc4f65069c0b502b5c65bf
Andrzej <andrzej.zuzak@coders-mill.com>
2015-07-20 14:38:42 +0200

    added base view and base presenter structure

--- app/src/main/java/com/example/andrzejz/kotlinproject/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/example/andrzejz/kotlinproject/extensions/ActivityExtensions.kt
@@ -2,9 +2,9 @@ package com.example.andrzejz.kotlinproject.extensions
 
 import android.support.design.widget.Snackbar
 import android.support.v7.app.ActionBarActivity
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import com.example.andrzejz.kotlinproject.SecondActivity
-import com.example.andrzejz.kotlinproject.network.api.Api
+import com.example.andrzejz.kotlinproject.network.api.ApiController
 import com.example.andrzejz.kotlinproject.network.traits.FakeApi
 import com.example.andrzejz.kotlinproject.singletons.Constants
 import org.jetbrains.anko.alert
@@ -14,12 +14,12 @@ import org.jetbrains.anko.intentFor
  * Created by Andrzej Z on 7/15/2015.
  */
 
-fun ActionBarActivity.showSnackBar(text: String, view: View) {
+fun AppCompatActivity.showSnackBar(text: String, view: View) {
     Snackbar
         .make(view, text, Snackbar.LENGTH_SHORT)
         .show()
 }
 
-fun ActionBarActivity.getApi(): FakeApi {
-    return Api.api
+fun AppCompatActivity.getApi(): FakeApi {
+    return ApiController.api
 }
